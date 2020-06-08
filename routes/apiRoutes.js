f0d9dec7651e94253fb41e882dfa0ced12d3491c
Sam Bonias <sbonias84@gmail.com>
2020-06-08 19:40:20 -0400

    removed the js and css folders, created basic HTML file, added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,11 @@
 const router = require("express").Router();
 
 // add our routes
+// route for saving activity data
+
+// route for getting activity data by duration
+
+// route for getting activity data by mileage
 
 // this is going to export the router that we created to include all
 // of our routes
