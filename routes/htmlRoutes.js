a68add7e54589e5beeeb5c1f41f3baca4db8d9a7
Sam Bonias <sbonias84@gmail.com>
2020-06-08 20:09:33 -0400

    added basic html structure and html homepage route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,17 @@
 // this is creating a new router object for us
 const router = require("express").Router();
 
+// module provided by node
+const path = require("path");
+
 // add our routes
+// HTML route for displaying the homepage
+router.get("/", (req, res) => {
+  // build your path variable
+  // .. is how we get back a directory
+  const filePath = path.join(__dirname, "..", "public", "html", "index.html");
+  res.sendFile(filePath);
+});
 
 // this is going to export the router that we created to include all
 // of our routes
